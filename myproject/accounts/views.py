4f343a3c78144425ea348f0e3f0cfbd8f6e2561f
Kaviya110103 <kaviyamuthusamy11@gmail.com>
2026-06-01 14:29:16 +0530

    Initial Django project

---
+++ myproject/accounts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+def home(request):
+    return render(request, "home.html")
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+        else:
+            User.objects.create_user(username=username, password=password)
+            messages.success(request, "Account created! Please login")
+
+    return redirect("home")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid login")
+
+    return redirect("home")
+
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
